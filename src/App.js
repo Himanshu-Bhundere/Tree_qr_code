cf6e9cc47efb4f83352411e12e3c20eaea72965b
ameybagwe12 <89726637+ameybagwe12@users.noreply.github.com>
2023-05-01 20:54:22 +0530

    removing hello

--- src/App.js
+++ src/App.js
@@ -7,18 +7,15 @@ function App() {
   return (
     <>
       <div className="App">
-        <div className="toolbar">
-          <h1>Hello Edit</h1>
-        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/qrcode" element={<Qrcode />} />
+          </Routes>
+          <Routes>
+            <Route path="/plant/:id" element={<Plant />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/qrcode" element={<Qrcode />} />
-        </Routes>
-        <Routes>
-          <Route path="/plant/:id" element={<Plant />} />
-        </Routes>
-      </BrowserRouter>
     </>
   );
 }
