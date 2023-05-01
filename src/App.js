df1ba0d6a744fe80411c654f12d62516eacfac81
ameybagwe12 <89726637+ameybagwe12@users.noreply.github.com>
2023-05-01 20:17:07 +0530

    checking changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import Qrcode from "./components/Qrcode";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-
 function App() {
   return (
     <>
       <div className="App">
         <div className="toolbar">
-          <h1>Hello</h1>
+          <h1>Hello Edit</h1>
         </div>
       </div>
       <BrowserRouter>
