7651a895120d503fffd5a25c81a589b07a5e9cae
ameybagwe12 <89726637+ameybagwe12@users.noreply.github.com>
2023-05-01 13:30:09 +0530

    qr-code commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
+import Qrcode from "./components/Qrcode";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+let data = require("./tree-data.json");
 
 function App() {
   return (
-    <div className="App">
-      <div className="toolbar">
-        <h1>Hello</h1>
+    <>
+      <div className="App">
+        <div className="toolbar">
+          <h1>Hello</h1>
+        </div>
       </div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/qrcode" element={<Qrcode />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Qrcode.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { QRCode } from "react-qrcode-logo";
+
+let data = require("../tree-data.json");
+export default function Qrcode() {
+  return (
+    <>
+      <div>
+        <div>
+          {data.map(function (object, i) {
+            return (
+              <QRCode
+                value="https://tree-tag-nssxcrce.netlify.app/"
+                logoImage={require("../nss-logo.png")}
+                logoWidth={80}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
