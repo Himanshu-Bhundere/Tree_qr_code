52a2b00a12071433fb4741cd3722917f7798d130
ameybagwe12 <89726637+ameybagwe12@users.noreply.github.com>
2023-05-03 22:03:55 +0530

    qrcode -display commit

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+let data = require("../tree-data.json");
+
 export default function Plant() {
   const { id } = useParams();
-  return <div>{id}</div>;
+  const filterData = data.find((obj) => {
+    return obj.id === id;
+  });
+
+  function callData() {
+    console.log(filterData);
+  }
+
+  return (
+    <>
+      <div onClick={callData}>
+        {id} - {filterData.name}
+      </div>
+    </>
+  );
 }
