ba7d06029d87baf8323026a8595f93220358ec1d
ameybagwe12 <89726637+ameybagwe12@users.noreply.github.com>
2023-05-05 00:48:01 +0530

    media queries and animations

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useLottie } from "lottie-react";
-import treeAnimation from "../animations/72702-tree.json";
-import dataAnimation from "../animations/102382-data.json";
-import mapAnimation from "../animations/47956-area-map.json";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import "./styles/Plants.css";
 
 let data = require("../tree-data.json");
 
@@ -13,49 +11,97 @@ export default function Plant() {
     return obj.id == id;
   });
 
-  const options = {
-    animationData: treeAnimation,
-    loop: true,
-  };
-
-  const options1 = {
-    animationData: dataAnimation,
-    loop: true,
-  };
-
-  const { View } = useLottie(options);
-  const { View2 } = useLottie(options1);
+  const href = `https://www.google.com/search?q=${filterData["botanical-name"]}`;
 
   const renderList = filterData.location.map((item, index) => (
-    <li key={index}>{item}</li>
+    <>
+      <li className="font" key={index}>
+        {item}
+      </li>
+      <br />
+    </>
   ));
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-        }}
-      >
-        <div style={{ height: 200, width: 200 }}>{View}</div>
+      <body>
         <div>
-          <h1>Id.No - {filterData.id}</h1>
-          <h2>Commom Name - {filterData.name}</h2>
-          <h2>Botanical Name - {filterData["botanical-name"]}</h2>
+          <div className="firstAni" style={{ height: 200, width: 200 }}>
+            <Player
+              autoplay
+              loop
+              src="https://assets7.lottiefiles.com/packages/lf20_edpg3c3s.json"
+              style={{ height: "300px", width: "300px" }}
+            >
+              <Controls
+                visible={false}
+                buttons={["play", "repeat", "frame", "debug"]}
+              />
+            </Player>
+          </div>
+          <div style={{ marginTop: 70 }}>
+            <h1 className="heading">Id.No - {filterData.id}</h1>
+            <h2 className="heading">Commom Name - {filterData.name}</h2>
+            <h2 className="heading">
+              Botanical Name - {filterData["botanical-name"]}
+            </h2>
+          </div>
         </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-        }}
-      >
-        <div style={{ height: 200, width: 200 }}>{View2}</div>
-      </div>
-      <ul>{renderList}</ul>
+        <hr className="hr" />
+        <div>
+          <div className="firstAni" style={{ height: 200, width: 200 }}>
+            <Player
+              autoplay
+              loop
+              src="https://assets7.lottiefiles.com/packages/lf20_tzv8osvb.json"
+              style={{ height: "300px", width: "300px" }}
+            >
+              <Controls
+                visible={false}
+                buttons={["play", "repeat", "frame", "debug"]}
+              />
+            </Player>
+          </div>
+
+          <div className="info">
+            <h1 className="heading">Information</h1>
+            <br />
+            {filterData["info"]}
+            <br />
+            <h3 className="font">
+              More Info on. -{" "}
+              <a target="blank" href={href}>
+                Visit here..
+              </a>
+            </h3>
+          </div>
+        </div>
+        <hr className="hr" />
+        <div>
+          <div className="firstAni" style={{ height: 200, width: 200 }}>
+            <Player
+              autoplay
+              loop
+              src="https://assets9.lottiefiles.com/packages/lf20_svy4ivvy.json"
+              style={{ height: "300px", width: "300px" }}
+            >
+              <Controls
+                visible={false}
+                buttons={["play", "repeat", "frame", "debug"]}
+              />
+            </Player>
+          </div>
+          <div>
+            <h1 className="heading loc">Other Locations(On Campus)</h1>
+            <ul style={{ listStyleType: "none" }}>
+              <div>
+                {renderList}
+                <br />
+              </div>
+            </ul>
+          </div>
+        </div>
+      </body>
     </>
   );
 }
